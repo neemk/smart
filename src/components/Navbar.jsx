266aa33ad058b1521e6f01a2e4aceb7e8c48c5d4
Animesh Kumar <animeshkumar272@gmail.com>
2023-07-18 00:24:43 +0530

    basic navbar ready

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,37 +7,37 @@ export default function Navbar() {
       <NavLink to="/" className="nav-brand">
         <img src={NikeLogo} />
       </NavLink>
-      <ul>
-        <li className="nav-item">
-          <NavLink
-            className={({ isActive }) => (isActive ? "active" : "")}
-            to="/"
-          >
-            Home
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            className={({ isActive }) => (isActive ? "active" : "")}
-            to="/about"
-          >
-            About us
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            className={({ isActive }) => (isActive ? "active" : "")}
-            to="/products"
-          >
-            Products
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/cart" className="nav-item nav-cart btn btn-accent">
-            Cart (0)
-          </NavLink>
-        </li>
-      </ul>
+      <div className="nav-items-wrapper">
+        <ul>
+          <li className="nav-item">
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "")}
+              to="/"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "")}
+              to="/about"
+            >
+              About us
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "")}
+              to="/products"
+            >
+              Products
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <NavLink to="/cart" className="nav-item nav-cart btn btn-accent">
+        Cart (0)
+      </NavLink>
     </nav>
   );
 }
