faa6d6754fa0d9503f5392a0e4f0d4e5adff896f
Animesh Kumar <animeshkumar272@gmail.com>
2023-07-24 22:24:08 +0530

    added toggle feature for filter section

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import FilterSection from "./FilterSection";
 import ProductList from "./ProductList";
 
 export default function Products(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
+      <div>
+        <span onClick={() => setIsOpen(!isOpen)}>Hide Filters</span>
+      </div>
       <div className="grid grid-filter-column pt-20">
-        <div>
-          <FilterSection />
-        </div>
+        {isOpen && (
+          <div className="filter-section">
+            <FilterSection />
+          </div>
+        )}
         <div className="products-view">
           <ProductList />
         </div>
