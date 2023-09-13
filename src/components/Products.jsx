732291f6b71abc821e9debc494a343970f0a921a
Animesh Kumar <animeshkumar272@gmail.com>
2023-09-14 01:23:15 +0530

    1. created MaxWidthWrapper component to give width to pages 2. modified filter section completely using tailwind

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,20 @@
-import { useState, forwardRef } from "react";
+import { useState, useEffect, forwardRef } from "react";
 
+import LinkFilters from "./LinkFilters";
 import FilterSection from "./FilterSection";
-import ProductList from "./ProductList";
+import Product from "./Product";
 import FilterDialog from "./FilterDialog";
+import MaxWidthWrapper from "./MaxWidthWrapper";
 
 export default function Products(props) {
   const [open, setOpen] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
 
   const handleClick = () => {
     setOpen(!open);
@@ -13,28 +22,48 @@ export default function Products(props) {
 
   return (
     <>
-      <div>
-        <span onClick={handleClick}>Hide Filters</span>
-      </div>
-
-      {open && (
-        <div>
-          <FilterDialog handleClick={handleClick}>
-            <FilterSection />
-          </FilterDialog>
+      <MaxWidthWrapper>
+        {/* <div className="my-5 flex h-10 w-full justify-center lg:justify-end">
+          <LinkFiltersButton />
+          <LinkSort />
+        </div> */}
+        <div className="grid grid-cols-1 gap-5 lg:grid-cols-7">
+          <div className="sticky top-32 col-span-2 hidden max-h-[calc(100vh-150px)] self-start overflow-auto rounded-lg bg-white lg:block">
+            <LinkFilters />
+          </div>
+          <div className="col-span-1 auto-rows-min grid-cols-1 lg:col-span-5 lg:pt-10">
+            <ul className="grid min-h-[66.5vh] grid-cols-3 gap-3">
+              {products.map((product) => {
+                return <Product key={product.id} details={product} />;
+              })}
+            </ul>
+            {/* {links && links.length > 0 && <LinkPagination />} */}
+          </div>
         </div>
-      )}
 
-      <div className="grid grid-filter-column pt-20">
+        {/* <div>
+          <span onClick={handleClick}>Hide Filters</span>
+        </div>
+
         {open && (
-          <div className="filter-section">
-            <FilterSection />
+          <div>
+            <FilterDialog handleClick={handleClick}>
+              <FilterSection />
+            </FilterDialog>
           </div>
         )}
-        <div className="products-view">
-          <ProductList />
-        </div>
-      </div>
+
+        <div className="grid grid-filter-column pt-20">
+          {open && (
+            <div className="filter-section">
+              <FilterSection />
+            </div>
+          )}
+          <div className="products-view">
+            <ProductList />
+          </div>
+        </div> */}
+      </MaxWidthWrapper>
     </>
   );
 }
