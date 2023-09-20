b933833299ccb0a4603f89a2561f47490b20835a
Animesh Kumar <animeshkumar272@gmail.com>
2023-09-21 01:23:45 +0530

    1. downloaded the original fonts 2. fixed cart page css 3. fixed product details page css

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,47 +1,66 @@
 import Button from "./Button";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+
+const sizes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 export default function ProductDetails(props) {
   return (
-    <div className="product-details-container">
-      <div className="product-details-layout">
-        <div>
-          <img
-            src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/b7d9211c-26e7-431a-ac24-b0540fb3c00f/air-force-1-07-shoes-WrLlWX.png"
-            className="product-details-image"
-            alt="Nike Motiva"
-          />
+    <div className="mt-24">
+      <div className="flex max-w-[1200px] mx-auto">
+        <div className="w-[calc(100%-456px)] mt-12">
+          <div className="max-h-[669px] min-h-[455px] h-[669px] pl-12">
+            <div className="w-[535px]">
+              <img
+                src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/b7d9211c-26e7-431a-ac24-b0540fb3c00f/air-force-1-07-shoes-WrLlWX.png"
+                className="rounded-lg"
+                alt="Nike Motiva"
+              />
+            </div>
+          </div>
         </div>
-        <div>
-          <div>
-            <p className="product-details-title">Nike Motiva</p>
-            <p className="product-details-subtitle">Men's Walking Shoes</p>
-            <span className="product-details-price">MRP:₹ 9 695</span>
-            <p className="text-light-gray">incl. of taxed</p>
-            <p className="text-light-gray">
-              (Also includes all applicable dues)
-            </p>
-
-            <div className="product-details--select-size">
-              <p className="text-semi-bold">Select Size</p>
-              <div className="product-details--sizes">
-                <Button className="btn--select-size">UK 5.5</Button>
-                <Button className="btn--select-size">UK 6</Button>
-                <Button className="btn--select-size">UK 7</Button>
-                <Button className="btn--select-size">UK 8</Button>
-                <Button className="btn--select-size">UK 9</Button>
-                <Button className="btn--select-size">UK 10</Button>
-                <Button className="btn--select-size">UK 10.5</Button>
+        <div className="w-[456px]">
+          <div className="mr-2 mt-12 pt-1 pr-12 pl-6">
+            <div>
+              <h1 className="text-2xl leading-5 font-bold">Air Jordan 1 Low</h1>
+              <h2 className="font-medium pb-1">Men's Shoes</h2>
+              <div>
+                <p className="font-medium mt-3">MRP : ₹ 9 995.00</p>
+                <p className="text-gray-500">incl. of taxes</p>
+                <p className="text-gray-500">
+                  (Also includes all applicable dues)
+                </p>
               </div>
             </div>
-
-            <Button className="btn--add-to-bag">Add to Bag</Button>
-
-            <p className="product-details-description">
-              The radiance lives on in the Nike Air Force 1 '07, the basketball
-              original that puts a fresh spin on what you know best: durably
-              stitched overlays, clean finishes and the perfect amount of flash
-              to make you shine.
-            </p>
+            <div className="mt-8">
+              <div className="mb-3">
+                <legend>todo</legend>
+                <div className="mt-2 grid grid-cols-3 gap-[7px]">
+                  {sizes.map((size) => {
+                    return (
+                      <div>
+                        <input
+                          type="radio"
+                          className="invisible absolute h-[1px] w-[1px]"
+                        />
+                        <label
+                          htmlFor=""
+                          className="bg-[#F7F7F7] text-[#DDDDDD] flex justify-center items-center h-[48px] border-none outline-none shadow-[rgb(229,229,229)_0_0_0_1px] rounded"
+                        >
+                          UK {size} (EU 40)
+                        </label>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div>
+                <div className="mt-1.5 mb-6">
+                  <button className="bg-[#111111] text-[#FFFFFF] w-full min-h-[58px] py-[18px] px-[24px] rounded-[30px]">
+                    Add To Bag
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
